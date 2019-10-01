5f9759a89af750158bd03e8839af15b0602aa961
charles gomes <charles2588@users.noreply.github.com>
2019-10-01 15:50:03 -0700

    Update sparksubmitjob.sh

--- AnalyticsEngine/sparksubmitjob.sh
+++ AnalyticsEngine/sparksubmitjob.sh
@@ -1,1 +1,1 @@
-spark-submit --master yarn --deploy-mode cluster --class org.apache.spark.examples.SparkPi /usr/hdp/current/spark2-client/jars/spark-examples.jar
+spark-submit --master yarn --deploy-mode cluster --class org.apache.spark.examples.SparkPi /usr/hdp/current/spark2-client/jars/spark-examples.jar >> /tmp/charlestest.txt
